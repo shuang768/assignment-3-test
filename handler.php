ac9f0fb1b774580aba58924746f05bc991662df2
shuang768 <113189677+shuang768@users.noreply.github.com>
2022-11-14 14:56:10 +0000

    done

--- handler.php
+++ handler.php
@@ -27,11 +27,7 @@
       
       <main class="mdl-layout__content" style="margin-left:75px;">
         <?php
-            if (isset($_POST['pizza_size'])&& $_POST['pizza_size']==1){
-            echo "<meta http-equiv=\"refresh\" content=\"0; url=?$m=$m+6.00\">";}
-            elseif (isset($_POST['pizza_size'])&& $_POST['pizza_size']==2){
-          $m=$m+10.00;}
-            elseif (isset($_POST['pizza_size'])&& $_POST['pizza_size']==0){
-          echo "order cancelled";}
-            else { echo "<meta http-equiv=\"refresh\" content=\"0; url=https://test.shuang768.repl.co?error=1\">";}?>
+$m=$_POST['pizza_size']+$_POST['toppings']+$m;
+echo "this order cost you ";
+echo round($m*1.13,2);?>
 </html>
--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <link rel="stylesheet" href="index.css">
     <link rel="manifest" href="./site.webmanifest">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <link rel="stylesheet" href="style.css">
@@ -25,18 +24,34 @@
         </div>
       </header>
       
-      <main class="mdl-layout__content" style="margin-left:75px;">
+        <main class="mdl-layout__content" style="margin-left:75px;">
+      <p text>Choose your pizza size</p>
+      <form  action="handler.php" method=post>
+      <input type="radio" id="large" name="pizza_size" value="6.00" required>
+      <label for="large">large $6.00</label><br>
+      <input type="radio" id="extra_large" name="pizza_size" value="10.00">
+      <label for="extra_large">extra large $10.00</label><br>
+      <input type="radio" id="cancel_order" name="pizza_size" value="0">
+      <label for="cancel_order">cancel order</label><br>
       <p text>Choose your pizza toppings</p>
-      <form  action="handler2.php" method=post>
-      <input type="radio" id="1t" name="toppings" value="1">
+      <input type="radio" id="1t" name="toppings" value="1.00" required>
       <label for="1t">1 toppings $1.00</label><br>
-      <input type="radio" id="2t" name="toppings" value="2">
+      <input type="radio" id="2t" name="toppings" value="1.75">
       <label for="2t">2 toppings $1.75</label><br>
-      <input type="radio" id="3t" name="toppings" value="3">
+      <input type="radio" id="3t" name="toppings" value="2.50">
       <label for="3t">3 toppings $2.50</label><br>
-      <input type="radio" id="4t" name="toppings" value="4">
+      <input type="radio" id="4t" name="toppings" value="3.25">
       <label for="4t">4 toppings $3.25</label><br>
       <input type="radio" id="0t" name="toppings" value="0">
       <label for="0t">cancel order</label><br><br>
+      <?php
+        if (isset($_GET) && $_GET['error'] == 1) {
+            echo "<p class=\"variablecolour\">please choose one</p>";
+          }
+          ?>
       <input type="submit" value="Submit">
-    </form>
+      </form>
+      
+        
+          
+  
